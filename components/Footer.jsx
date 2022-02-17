46df3771bd4a15419051392911d38ede75b4555c
nicojoaquin <nicojoaquin1998@gmail.com>
2022-02-17 18:44:42 -0300

    fb

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
           <article className={styles.red}>
             <Image width={50} height={50} src={facebook} alt="facebook" />
             <br />
-            <span>Visitanos Facebook</span>
+            <span>Visitanos en Facebook</span>
           </article>
         </a>
       </div>
